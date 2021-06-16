88fa990835d1acdb5a5ffb01873bc8e5a9771f95
snorlax-22 <manhcuong25710@gmail.com>
2021-06-16 23:03:56 +0700

    basic alarm clocl v1.0

---
+++ alarm_clock.py
@@ -0,0 +1,22 @@
+import datetime
+
+from playsound import playsound
+
+alarmHour = int(input("What time do you want to wake up ?"))
+alarmMinute = int(input("What minute do you want to wake up ?"))
+amPM = str(input("am or pm ?"))
+
+print("Waiting for alarm",alarmHour,alarmMinute,amPM)
+
+if(amPM == "pm") :
+	alarmHour = alarmHour + 12
+
+while(True):
+	if(alarmHour == datetime.datetime.now().hour and
+		alarmMinute == datetime.datetime.now().minute):
+		print("Wake up !!!")
+		playsound("Ringtone.mp3")
+		break
+
+print("exited")
+
